236d2e5a741170a4af14a7a76e19a6873c6cca77
essss <sousououederni@gmail.com>
2023-05-14 23:15:07 +0100

    model

--- models/book.model.js
+++ models/book.model.js
@@ -1,1 +1,26 @@
 //book model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let Book = new Schema({
+  book_title: {
+    type: String,
+  },
+  book_author: {
+    type: String,
+  },
+  book_description: {
+    type: String,
+  },
+  book_year_published: {
+    type: String,
+  },
+  book_genre: {
+    type: String,
+  },
+  book_isbn: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Book", Book);
