7fd47f60fe3bb7bf6b43060d50cdb01eaaf83960
Chimamanda16 <Chimamandaunachukwu@gmail.com>
2023-05-15 15:50:54 +0100

    Handle post route

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,1 +1,38 @@
 //index controller
+const app = require("../index.js");
+const express = require('express');
+const Author = require("../models/author.model.js");
+const Book = require("../models/book.model.js");
+app.use(express.urlencoded({extended: true}));
+
+function createBook(req, res){
+    const newAuthor = new Author({
+        author_name: req.body.author_name,
+          author_age: req.body.author_age,
+    });
+    const newBook = new Book({
+        book_title: req.body.book_title,
+        book_author: newAuthor._id,
+        book_description: req.body.book_description,
+        book_year_published: req.body.book_year_published,
+        book_genre: req.body.book_genre,
+        book_isbn: req.body.book_isbn,
+    });
+    newAuthor.author_books.push(newBook._id);
+
+    newBook.save().then((response) => {
+        res.send("Added successfully!");
+        console.log(response);
+    }).catch((err) =>{
+        console.log(err);
+    });
+    newAuthor.save().then((response) => {
+        // res.send("Added successfully!");
+        console.log(response);
+    }).catch((err) =>{
+        console.log(err);
+    });
+    
+}
+
+module.exports = createBook;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const router = express.Router();
 const Book = require("../models/book.model.js");
-
+const createBook = require("../controllers/index.controller.js");
 router.get('/', (req, res) => {
     console.log('Connecting');
     Book.find().then((response) =>{
         res.send(response);
     }); 
 });
+
+router.post("/", createBook);
 
 module.exports = router;
